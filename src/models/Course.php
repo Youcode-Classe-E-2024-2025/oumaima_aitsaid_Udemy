b793b1b43cabbe52e23b061ffe31bf418ee77dd5
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-15 09:11:05 +0100

    ajouter methode getCourse with categorie and teacher

--- src/models/Course.php
+++ src/models/Course.php
@@ -12,7 +12,19 @@ class Course {
     public function __construct($db) {
         $this->conn = $db;
     }
-   
+    public function getCourses($limit, $offset) {
+        $query = "SELECT c.id, c.title, c.description, c.created_at, u.username AS teacher_name, cat.name AS category
+                  FROM cours c
+                  LEFT JOIN utilisateurs u ON c.teacher_id = u.id
+                  LEFT JOIN categories cat ON c.category_id = cat.id
+                  ORDER BY c.created_at DESC
+                  LIMIT :limit OFFSET :offset";
+        $stmt = $this->conn->prepare($query);
+                $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+        $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
    
 }
