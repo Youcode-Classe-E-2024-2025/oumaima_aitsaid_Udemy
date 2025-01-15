6c321f3d6b5e31d8eb6a3d047b5dc800bc1f4dc6
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-15 09:13:18 +0100

    ajouter method index pour la pagination

--- src/controllers/CourseController.php
+++ src/controllers/CourseController.php
@@ -9,7 +9,16 @@ class CourseController {
     public function listCourses() {
         return $this->courseModel->getAll();
     }
-
+public function index() {
+    $coursesPerPage = 10;
+    $currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+    $currentPage = max($currentPage, 1);
+    $offset = ($currentPage - 1) * $coursesPerPage;
+    $courses = $this->courseModel->getCourses($coursesPerPage, $offset);
+    $totalCourses = $this->courseModel->countCourses();
+    $totalPages = ceil($totalCourses / $coursesPerPage);
+    include('views/courseListView.php');
+}
 
    
 
