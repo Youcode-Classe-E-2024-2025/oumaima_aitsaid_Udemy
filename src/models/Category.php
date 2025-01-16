b43a6495cf3aa936e6b9270b53e9b7cfe4ff5ad4
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-16 17:14:02 +0100

    ajouter function getById

--- src/models/Category.php
+++ src/models/Category.php
@@ -18,5 +18,13 @@
      $stmt->bindParam(':name', $name);
      return $stmt->execute();
     }
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<getbyIdCategories>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+    public function getById($id) {
+        $query = "SELECT * FROM " . $this->table . " WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
    
