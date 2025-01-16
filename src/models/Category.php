44e104772f6f7ac5536cab2605b1cac536076215
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-16 17:13:01 +0100

    adding class category in model with method getall

--- src/models/Category.php
+++ src/models/Category.php
@@ -5,5 +5,11 @@
     public function __construct($db) {
         $this->conn = $db;
     }
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<getAllCategories>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+    public function getAll() {
+        $query = "SELECT * FROM " . $this->table;
+        $stmt = $this->conn->query($query);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
    
