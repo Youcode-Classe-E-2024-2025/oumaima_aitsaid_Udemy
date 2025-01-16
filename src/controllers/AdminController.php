d22299662198ef2396e0ed976c967459fc359eab
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-16 17:09:33 +0100

    ajouter function validateTeacher in adminController

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -17,6 +17,20 @@ class AdminController {
         $stats = $this->admin->getStatistics();
         include __DIR__ . '/../../Views/admin_dashboard.php';
     }
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<validateTeacher>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+    public function validateTeacher() {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $teacher_id = filter_input(INPUT_POST, 'teacher_id', FILTER_SANITIZE_NUMBER_INT);
+            if ($teacher_id && $this->admin->validateTeacher($teacher_id)) {
+                header("Location: index.php?action=admin_dashboard&message=validated");
+                exit();
+            } else {
+                header("Location: index.php?action=admin_dashboard&message=error");
+                exit();
+            }
+        }
+    }
 
     
 }
