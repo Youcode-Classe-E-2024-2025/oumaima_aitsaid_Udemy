fdfd9f37b4a538807d879f591f8cb171bd11b8d9
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-17 09:17:28 +0100

    ajouter function getById tag

--- src/models/Tag.php
+++ src/models/Tag.php
@@ -43,5 +43,20 @@
         }
     }
     
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<getbyid>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+    public function getById($id) {
+        try {
+            $query = "SELECT * FROM " . $this->table . " WHERE id = :id";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC); 
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        }
+    }
+    
     
 }
