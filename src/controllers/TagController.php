7a276d12fc3cd770c59c707f0a8aa6bd239816e3
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-17 09:14:04 +0100

    ajouter function create Tag

--- src/controllers/TagController.php
+++ src/controllers/TagController.php
@@ -15,7 +15,24 @@ class TagsController {
         include 'Views/list_tags.php';
     }
 
-  
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<CreateTags>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+    public function createTag() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $names = trim($_POST['name']); 
+            if (!empty($names)) {
+                $tags = array_map('trim', explode(',', $names));
+                $tags = array_filter($tags);
+                if (!empty($tags)) {
+                    $this->tagsModel->createBulk($tags); 
+                    header("Location: index.php?action=Tags&message=created");
+                }
+            }
+            $error_message = "At least one tag is required.";
+        }
+        $tags = $this->tagsModel->getAll();
+        include 'Views/list_tags.php';
+    }
 
 
 
