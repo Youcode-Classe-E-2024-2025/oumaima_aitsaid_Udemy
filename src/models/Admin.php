eb0460f6969b984dad6978fb79629035856d7528
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-17 09:19:07 +0100

    ajouter function validateTeacher in model Admin

--- src/models/Admin.php
+++ src/models/Admin.php
@@ -5,7 +5,14 @@ class Admin extends User {
         parent::__construct($db);
     }
 
-  
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<validateCompte Teacher>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+    public function validateTeacher($teacher_id) {
+        $query = "UPDATE utilisateurs SET validated= 1 WHERE id = :teacher_id AND role = 'teacher'";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':teacher_id', $teacher_id);
+        return $stmt->execute();
+    }
  
 
 
