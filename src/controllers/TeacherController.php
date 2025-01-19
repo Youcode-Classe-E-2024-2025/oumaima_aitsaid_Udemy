fe2b2d2561f9dd8b0c53156f3ac0521c5d770544
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-19 20:56:18 +0100

    add class teacherController

---
+++ src/controllers/TeacherController.php
@@ -0,0 +1,43 @@
+<?php
+
+class TeacherController {
+    private $db;
+    private $user;
+    private $course;
+
+    public function __construct($db) {
+        $this->db = $db;
+        $this->user = new User($db);
+        $this->course = new Course($db);
+        $this->categorie =new Category($db);
+        $this->Tags =new Tag($db);
+    }
+
+    
+   
+
+  
+
+
+    
+    
+
+   
+    
+  
+
+   
+
+ 
+   
+    
+    
+
+    
+
+    
+    
+
+
+
+}
