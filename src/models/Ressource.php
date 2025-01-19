57b76f982183ad8264ddf58154873a0861f481b4
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-19 20:48:12 +0100

    add class Ressource

--- src/models/Ressource.php
+++ src/models/Ressource.php
@@ -1,1 +1,11 @@
+<?php
+abstract class Ressource {
+    protected $title;
+    protected $file_path;
 
+    public function __construct($title, $file_path) {
+        $this->title = $title;
+        $this->file_path = $file_path;
+    }
+    abstract public function save($db, $course_id);
+}
