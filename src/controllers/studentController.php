c7bf0c06cb2d70c081380972beaf4bcfb09ffff9
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-20 17:14:53 +0100

    ajouter methode displayCourse  in class studentController

--- src/controllers/studentController.php
+++ src/controllers/studentController.php
@@ -15,5 +15,22 @@ class studentController{
 
 
 
+public function displayCourse($id) {
+    session_start();
+    if (!isset($_SESSION['user_id']) || !$this->user->isStudent($_SESSION['user_id'])) {
+        header("Location: login.php");
+        exit();
+    }
 
+    $course = $this->course->displayCourse($id);
+
+    if (!$course) {
+        header("Location: index.php?action=dashboard&error=course_not_found");
+        exit();
+    }
+        $resources = $this->course->getCourseResources($id);
+    $course['resources'] = $resources ?: []; 
+
+    include __DIR__ . '/../../views/course/display_courseStudent.php';
+}
 }
