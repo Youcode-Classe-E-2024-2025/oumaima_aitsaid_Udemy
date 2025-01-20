fe03501e94e2888305d97925a568a35911871b9e
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-20 17:13:43 +0100

    ajouter methode getUserCourses in class enrallement

--- src/models/EnrollmentModel.php
+++ src/models/EnrollmentModel.php
@@ -17,5 +17,15 @@ class EnrollmentModel {
         $query = $this->db->prepare("INSERT INTO inscriptions (student_id, course_id) VALUES (?, ?)");
         $query->execute([$userId, $courseId]);
     }
-   
+    public function getUserCourses($userId) {
+        $query = $this->db->prepare("
+            SELECT c.*, cat.name as category
+            FROM cours c
+            JOIN categories cat ON c.category_id = cat.id
+            JOIN inscriptions i ON c.id = i.course_id
+            WHERE i.student_id = ?       ");
+            $query->execute([$userId]);
+             return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
 }
