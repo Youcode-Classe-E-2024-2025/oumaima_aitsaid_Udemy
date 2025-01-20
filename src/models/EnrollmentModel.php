1e16603e1c208deabcdf8bacc8afcf7cf7c768f2
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-20 17:12:55 +0100

    add class enrollement with methode isEnrolled

--- src/models/EnrollmentModel.php
+++ src/models/EnrollmentModel.php
@@ -7,7 +7,11 @@ class EnrollmentModel {
         $this->db = $db;
     }
 
-   
+    public function isEnrolled($userId, $courseId) {
+        $query = $this->db->prepare("SELECT * FROM inscriptions WHERE student_id = ? AND course_id = ?");
+        $query->execute([$userId, $courseId]);
+        return $query->rowCount() > 0;
+    }
     
    
    
